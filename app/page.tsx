452455ba7f66510fcb61bb999ca9271a14f6b468
asif-munshi <asifmunshe2978@gmail.com>
2024-02-29 17:51:41 +0600

    added shadcn ui component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <span>Hello World!</span>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>Click me</Button>
+    </div>
   );
 }
