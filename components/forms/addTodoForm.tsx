c1b55786fb5db9770e3efbb1cfac6a482da0cadf
asif-munshi <asifmunshe2978@gmail.com>
2024-03-02 16:29:31 +0600

    add✨ add todo page with react-hook-form and zod

---
+++ components/forms/addTodoForm.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+const formSchema = z
+  .object({
+    title: z.string().min(1, { message: 'Title Required' }).max(255),
+    description: z.string(),
+  })
+  .required({
+    title: true,
+  });
+
+const AddTodoForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    form.reset({
+      title: '',
+      description: '',
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Why react sucks?"
+                  className="focus-visible:ring-offset-0"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Because it sucks?"
+                  className="resize-none focus-visible:ring-offset-0"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-end">
+          <Button type="submit" className="rounded-full">
+            Add Todo
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default AddTodoForm;
