494cd98ab2aae843f2ba5f4accd3e88bd5526981
asif-munshi <asifmunshe2978@gmail.com>
2024-03-17 13:28:26 +0600

    bumped package version

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -59,6 +59,14 @@ const Navbar = () => {
           <NavigationMenuItem>
             <ModeToggle />
           </NavigationMenuItem>
+          {/* Get Started */}
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-green-700 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-green-800 hover:text-white focus:bg-green-800 focus:text-white focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                Get Started
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
