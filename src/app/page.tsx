54c7878ed664b5ecf62d50108284487c9b5548ff
asif-munshi <asifmunshe2978@gmail.com>
2024-04-20 02:58:17 +0600

    add✨shadcn/ui re-usable components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="font-mono text-2xl">Hello World!</div>
+      <Button>Click me</Button>
     </main>
   );
 }
